dc01cacc4c785ed5508b4c18d7a76be5fac0e7a0
Chernyavskiy0465 <61347944+Chernyavskiy0465@users.noreply.github.com>
2023-01-17 11:50:34 +0200

    Delete task4 directory

--- task4/provision.sh
+++
@@ -1,8 +0,0 @@
-sudo useradd -m -p adminuser adminuser 
-sudo usermod -aG sudo adminuser 
-
-sudo useradd -m -G sudo -p nopassword poweruser
-echo 'poweruser ALL=(ALL) NOPASSWD:' >> /etc/sudo 
-echo 'poweruser ALL=(ALL) NOPASSWD:' >> /usr/sbin/iptables
-sudo usermod -a -G adminuser poweruser
-sudo ln -s /etc/mtab /home/poweruser/mylink
